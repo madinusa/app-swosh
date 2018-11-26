c778b367b2d1b1d2cd3407783818432f99f7d133
Rasmus Andersson <rasmusandersson@Rasmus-Genesis-IT.local>
2018-11-26 22:07:35 +0100

    fixade startsidan

---
+++ app-swosh/app-swosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swosh
+//
+//  Created by Rasmus Andersson on 2018-11-26.
+//  Copyright © 2018 Rasmus Andersson. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
